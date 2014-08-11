Koha-Community/Koha

commit 6973e3b7d50777a147a29f2f3d213a6175c9eb74

Bug 11975: (follow-up) simplify construction of params for GetBorrowersToExpunge()
This patch consolidates the code for constructing the parameters for
GetBorrowersToExpunge() into a subroutine.  As a side-effect, this
fixes a regression introduced by the main patch where no patrons would
be deleted when filtering by expiration date.

To test:

[1] Follow the test plan in the main patch.

Signed-off-by: Galen Charlton <gmc@esilibrary.com>
Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
Signed-off-by: Galen Charlton <gmc@esilibrary.com>

tools/cleanborrowers.pl
@@ -85,39 +85,7 @@ if ( $step == 2 ) {
     my $membersToDelete;
     if ( $checkboxes{borrower} ) {
         $membersToDelete = GetBorrowersToExpunge(
-            {
-                (
-                    $not_borrowered_since
-                    ? (
-                        not_borrowered_since => output_pref(
-                            {
-                                dt         => $not_borrowered_since,
-                                dateformat => 'iso',
-                                dateonly   => 1
-                            }
-                        )
-                      )
-                    : ()
-                ),
-                (
-                    $borrower_dateexpiry
-                    ? (
-                        expired_before => output_pref(
-                            {
-                                dt         => $borrower_dateexpiry,
-                                dateformat => 'iso',
-                                dateonly   => 1
-                            }
-                        )
-                      )
-                    : ()
-                ),
-                (
-                    $borrower_categorycode
-                    ? ( category_code => $borrower_categorycode )
-                    : ()
-                ),
-            }
+            _get_selection_params($not_borrowered_since, $borrower_dateexpiry, $borrower_categorycode)
         );
         _skip_borrowers_with_nonzero_balance( $membersToDelete );
         $totalDel = scalar @$membersToDelete;
@@ -147,34 +115,7 @@ elsif ( $step == 3 ) {
     # delete members
     if ($do_delete) {
         my $membersToDelete = GetBorrowersToExpunge(
-            {
-                (
-                    $not_borrowered_since
-                    ? (
-                        not_borrowered_since => output_pref(
-                            {
-                                dt         => $not_borrowered_since,
-                                dateformat => 'iso'
-                            }
-                        )
-                      )
-                    : ()
-                ),
-                (
-                    $borrower_dateexpiry
-                    ? (
-                        expired_before => output_pref(
-                            { dt => $borrower_dateexpiry, dateformat => 'iso' }
-                        )
-                      )
-                    : ()
-                ),
-                (
-                    $borrower_categorycode
-                    ? ( category_code => $borrower_categorycode )
-                    : ()
-                ),
-            }
+            _get_selection_params($not_borrowered_since, $borrower_dateexpiry, $borrower_categorycode)
         );
         _skip_borrowers_with_nonzero_balance( $membersToDelete );
         $totalDel = scalar(@$membersToDelete);
@@ -227,3 +168,22 @@ sub _skip_borrowers_with_nonzero_balance {
         ($balance != 0) ? (): ($_);
     } @$borrowers;
 }
+
+sub _get_selection_params {
+    my ($not_borrowered_since, $borrower_dateexpiry, $borrower_categorycode) = @_;
+
+    my $params = {};
+    $params->{not_borrowered_since} = output_pref({
+        dt         => $not_borrowered_since,
+        dateformat => 'iso',
+        dateonly   => 1
+    }) if $not_borrowered_since;
+    $params->{expired_before} = output_pref({
+        dt         => $borrower_dateexpiry,
+        dateformat => 'iso',
+        dateonly   => 1
+    }) if $borrower_dateexpiry;
+    $params->{borrower_categorycode} = $borrower_categorycode if $borrower_categorycode;
+
+    return $params;
+};

KohaAloha/Koha2

commit 0f91ba4662c77d59b588e26bc67f06543e628ce1

Bug 12727 - Patron category filter in batch patron deletion tool is broken
After commit 6973e3b7d50777a147a29f2f3d213a6175c9eb74, patron category
filter in batch patron deletion tool (tools/cleanborrowers.pl) doesn't
work properly any longer. It's still possible to choose a patron
category as selection criterion, but this criterion is being
efectivelly ignored on futhers steps.

As a result, all patrons (with no check-outs and no outstanding
account balance) may get mistakenly deleted when using this tool,
despite their respective categories.

To test:

1/ apply patch
2/ follow the test plan[s] for Bug 11975

Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
Works as described, no problems found.

Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

tools/cleanborrowers.pl
@@ -183,7 +183,7 @@ sub _get_selection_params {
         dateformat => 'iso',
         dateonly   => 1
     }) if $borrower_dateexpiry;
-    $params->{borrower_categorycode} = $borrower_categorycode if $borrower_categorycode;
+    $params->{category_code} = $borrower_categorycode if $borrower_categorycode;
 
     return $params;
 };