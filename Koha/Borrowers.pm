MagnusEnger/koha

Bug 13019 - Add base classes on which to build Koha objects
The idea behind this is to have a pair of base classes on which to build
our new generation of Koha objects. Koha::Object is a base class, which
in it's most basic form, is to represent a row in a table. For example,
Koha::Borrower inherits from Koha::Object. So too could Koha::Biblio
and Koha::Item for example.

Koha::Objects is to represent a way to fetch and manipulate sets of
objects. For example, Koha::Borrowers has a method to get a
Koha::Borrower object by id and a method to search for an get a list
of Koha::Borrower objects. Right now Koha::Objects has only the
essentials but can easily be extended and those enhancements will be
passed down to all the child classes based on it.

By using these classes as a base, we will add consistency to our
code, allow us to keep our code DRY, reduce bugs, and encapsulate our
database access among other benefits.

Test Plan:
1) Apply this patch
2) prove t/Object.t t/db_dependent/Object.t t/db_dependent/Objects.t

Koha/Borrowers.pm
@@ -1,0 +1,58 @@
+package Koha::Borrowers;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use Koha::Borrower;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::Borrower - Koha Borrower Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub Type {
+    return 'Borrower';
+}
+
+sub ObjectClass {
+    return 'Koha::Borrower';
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

Koha/Object.pm
@@ -1,0 +1,286 @@
+package Koha::Object;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+use Encode qw{encode};
+
+use Koha::Database;
+
+=head1 NAME
+
+Koha::Object - Koha Object base class
+
+=head1 SYNOPSIS
+
+    use Koha::Object;
+    my $object = Koha::Object->new({ property1 => $property1, property2 => $property2, etc... } );
+
+=head1 DESCRIPTION
+
+This class must always be subclassed.
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 Koha::Object->new();
+
+my $object = Koha::Object->new();
+my $object = Koha::Object->new($attributes);
+
+=cut
+
+sub new {
+    my ( $class, $attributes ) = @_;
+    my $self = {};
+
+    if ($attributes) {
+        $self->{_result} =
+          Koha::Database->new()->schema()->resultset( $class->Type() )
+          ->new($attributes);
+    }
+
+    croak("No type found! Koha::Object must be subclassed!")
+      unless $class->Type();
+
+    bless( $self, $class );
+
+}
+
+=head3 Koha::Object->new_from_dbic();
+
+my $object = Koha::Object->new_from_dbic($dbic_row);
+
+=cut
+
+sub new_from_dbic {
+    my ( $class, $dbic_row ) = @_;
+    my $self = {};
+
+    # DBIC result row
+    $self->{_result} = $dbic_row;
+
+    croak("No type found! Koha::Object must be subclassed!")
+      unless $class->Type();
+
+    croak( "DBIC Result type isn't of the type " . $class->Type() )
+      unless ref( $self->{_result} ) eq "Koha::Schema::Result::"
+      . $class->Type();
+
+    bless( $self, $class );
+
+}
+
+=head3 $object->Store();
+
+Saves the object in storage.
+If the object is new, it will be created.
+If the object previously existed, it will be updated.
+
+Returns:
+    1  if the store was a success
+    0  if the store failed
+
+=cut
+
+sub Store {
+    my ($self) = @_;
+
+    return $self->_Result()->update_or_insert() ? 1 : 0;
+}
+
+=head3 $object->IsStore();
+
+Returns true if the object has been previously stored.
+
+=cut
+
+sub InStorage {
+    my ($self) = @_;
+
+    return $self->_Result()->in_storage();
+}
+
+=head3 $object->IsChanged();
+
+Returns true if the object has properties that are different from
+the properties of the object in storage.
+
+=cut
+
+sub IsChanged {
+    my ( $self, @columns ) = @_;
+
+    return $self->_Result()->is_changed(@columns);
+}
+
+=head3 $object->Delete();
+
+Removes the object from storage.
+
+Returns:
+    1  if the deletion was a success
+    0  if the deletion failed
+    -1 if the object was never in storage
+
+=cut
+
+sub Delete {
+    my ($self) = @_;
+
+    # Deleting something not in storage thows an exception
+    return -1 unless $self->_Result()->in_storage();
+
+    # Return a boolean for succcess
+    return $self->_Result()->delete() ? 1 : 0;
+}
+
+=head3 $object->Set( $properties_hashref )
+
+$object->Set(
+    {
+        property1 => $property1,
+        property2 => $property2,
+        property3 => $propery3,
+    }
+);
+
+Enables multiple properties to be set at once
+
+Returns:
+    1      if all properties were set.
+    0      if one or more properties do not exist.
+    undef  if all properties exist but a different error
+           prevents one or more properties from being set.
+
+If one or more of the properties do not exist,
+no properties will be set.
+
+=cut
+
+sub Set {
+    my ( $self, $properties ) = @_;
+
+    my @columns = @{$self->_Columns()};
+
+    foreach my $p ( keys %$properties ) {
+        unless ( $p ~~ @columns ) {
+            carp("No property $p!");
+            return 0;
+        }
+    }
+
+    return $self->_Result()->set_columns($properties) ? 1 : undef;
+}
+
+=head3 $object->Id();
+
+Returns the id of the object if it has one.
+
+=cut
+
+sub Id {
+    my ($self) = @_;
+
+    my ( $id ) = $self->_Result()->id();
+
+    return $id;
+}
+
+=head3 $object->_Result();
+
+Returns the internal DBIC Row object
+
+=cut
+
+sub _Result {
+    my ($self) = @_;
+
+    # If we don't have a dbic row at this point, we need to create an empty one
+    $self->{_result} ||=
+      Koha::Database->new()->schema()->resultset( $self->Type() )->new({});
+
+    return $self->{_result};
+}
+
+=head3 $object->_Columns();
+
+Returns an arrayref of the table columns
+
+=cut
+
+sub _Columns {
+    my ($self) = @_;
+
+    # If we don't have a dbic row at this point, we need to create an empty one
+    $self->{_columns} ||= [ $self->_Result()->result_source()->columns() ];
+
+    return $self->{_columns};
+}
+
+
+=head3 AUTOLOAD
+
+The autoload method is used only to get and set values for an objects properties.
+
+=cut
+
+sub AUTOLOAD {
+    my $self = shift;
+
+    my $method = our $AUTOLOAD;
+    $method =~ s/.*://;
+
+    my @columns = @{$self->_Columns()};
+    # Using direct setter/getter like $item->barcode() or $item->barcode($barcode);
+    if ( $method ~~ @columns ) {
+        if ( @_ ) {
+            return $self->_Result()->set_column( $method, @_ );
+        } else {
+            my $value = $self->_Result()->get_column( $method );
+            return encode( 'UTF-8', $value );
+        }
+    }
+
+    carp "No method $method!";
+    return;
+}
+
+=head3 Type
+
+This method must be defined in the child class. The value is the name of the DBIC resultset.
+For example, for borrowers, the Type method will return "Borrower".
+
+=cut
+
+sub Type { }
+
+sub DESTROY { }
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

t/db_dependent/Borrower.t
@@ -1,0 +1,70 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Test::More tests => 12;
+use Test::Warn;
+
+use C4::Context;
+use Koha::Database;
+
+BEGIN {
+    use_ok('Koha::Object');
+    use_ok('Koha::Borrower');
+}
+
+# Start transaction
+my $dbh = C4::Context->dbh;
+$dbh->{AutoCommit} = 0;
+$dbh->{RaiseError} = 1;
+
+my $categorycode = Koha::Database->new()->schema()->resultset('Category')->first()->categorycode();
+my $branchcode = Koha::Database->new()->schema()->resultset('Branch')->first()->branchcode();
+
+my $object = Koha::Borrower->new();
+
+is( $object->InStorage, 0, "Object is not in storage" );
+
+$object->categorycode( $categorycode );
+$object->branchcode( $branchcode );
+$object->surname("Test Surname");
+$object->Store();
+
+my $borrower = Koha::Database->new()->schema()->resultset('Borrower')->find( $object->borrowernumber() );
+is( $borrower->surname(), "Test Surname", "Object found in database" );
+
+is( $object->InStorage, 1, "Object is now stored" );
+
+is( $object->IsChanged(), 0, "Object is unchanged" );
+$object->surname("Test Surname 2");
+is( $object->IsChanged(), 1, "Object is changed" );
+
+$object->Store();
+is( $object->IsChanged(), 0, "Object no longer marked as changed after being stored" );
+
+$object->Set({ firstname => 'Test Firstname' });
+is( $object->IsChanged(), 1, "Object is changed after Set" );
+$object->Store();
+is( $object->IsChanged(), 0, "Object no longer marked as changed after being stored" );
+
+$object->Delete();
+$borrower = Koha::Database->new()->schema()->resultset('Borrower')->find( $object->borrowernumber() );
+ok( ! $borrower, "Object no longer found in database" );
+is( $object->InStorage, 0, "Object is not in storage" );
+
+1;

KohaAloha/Koha2

Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

Koha/Borrowers.pm
@@ -1,0 +1,58 @@
+package Koha::Borrowers;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use Koha::Borrower;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::Borrower - Koha Borrower Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub type {
+    return 'Borrower';
+}
+
+sub object_class {
+    return 'Koha::Borrower';
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

Koha/Object.pm
@@ -1,0 +1,285 @@
+package Koha::Object;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+use Encode qw{encode};
+
+use Koha::Database;
+
+=head1 NAME
+
+Koha::Object - Koha Object base class
+
+=head1 SYNOPSIS
+
+    use Koha::Object;
+    my $object = Koha::Object->new({ property1 => $property1, property2 => $property2, etc... } );
+
+=head1 DESCRIPTION
+
+This class must always be subclassed.
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 Koha::Object->new();
+
+my $object = Koha::Object->new();
+my $object = Koha::Object->new($attributes);
+
+=cut
+
+sub new {
+    my ( $class, $attributes ) = @_;
+    my $self = {};
+
+    if ($attributes) {
+        $self->{_result} =
+          Koha::Database->new()->schema()->resultset( $class->type() )
+          ->new($attributes);
+    }
+
+    croak("No type found! Koha::Object must be subclassed!")
+      unless $class->type();
+
+    bless( $self, $class );
+
+}
+
+=head3 Koha::Object->new_from_dbic();
+
+my $object = Koha::Object->new_from_dbic($dbic_row);
+
+=cut
+
+sub new_from_dbic {
+    my ( $class, $dbic_row ) = @_;
+    my $self = {};
+
+    # DBIC result row
+    $self->{_result} = $dbic_row;
+
+    croak("No type found! Koha::Object must be subclassed!")
+      unless $class->type();
+
+    croak( "DBIC result type " . ref( $self->{_result} ) . " isn't of the type " . $class->type() )
+      unless ref( $self->{_result} ) eq "Koha::Schema::Result::" . $class->type();
+
+    bless( $self, $class );
+
+}
+
+=head3 $object->store();
+
+Saves the object in storage.
+If the object is new, it will be created.
+If the object previously existed, it will be updated.
+
+Returns:
+    1  if the store was a success
+    0  if the store failed
+
+=cut
+
+sub store {
+    my ($self) = @_;
+
+    return $self->_result()->update_or_insert() ? 1 : 0;
+}
+
+=head3 $object->in_storage();
+
+Returns true if the object has been previously stored.
+
+=cut
+
+sub in_storage {
+    my ($self) = @_;
+
+    return $self->_result()->in_storage();
+}
+
+=head3 $object->is_changed();
+
+Returns true if the object has properties that are different from
+the properties of the object in storage.
+
+=cut
+
+sub is_changed {
+    my ( $self, @columns ) = @_;
+
+    return $self->_result()->is_changed(@columns);
+}
+
+=head3 $object->delete();
+
+Removes the object from storage.
+
+Returns:
+    1  if the deletion was a success
+    0  if the deletion failed
+    -1 if the object was never in storage
+
+=cut
+
+sub delete {
+    my ($self) = @_;
+
+    # Deleting something not in storage thows an exception
+    return -1 unless $self->_result()->in_storage();
+
+    # Return a boolean for succcess
+    return $self->_result()->delete() ? 1 : 0;
+}
+
+=head3 $object->set( $properties_hashref )
+
+$object->set(
+    {
+        property1 => $property1,
+        property2 => $property2,
+        property3 => $propery3,
+    }
+);
+
+Enables multiple properties to be set at once
+
+Returns:
+    1      if all properties were set.
+    0      if one or more properties do not exist.
+    undef  if all properties exist but a different error
+           prevents one or more properties from being set.
+
+If one or more of the properties do not exist,
+no properties will be set.
+
+=cut
+
+sub set {
+    my ( $self, $properties ) = @_;
+
+    my @columns = @{$self->_columns()};
+
+    foreach my $p ( keys %$properties ) {
+        unless ( $p ~~ @columns ) {
+            carp("No property $p!");
+            return 0;
+        }
+    }
+
+    return $self->_result()->set_columns($properties) ? 1 : undef;
+}
+
+=head3 $object->id();
+
+Returns the id of the object if it has one.
+
+=cut
+
+sub id {
+    my ($self) = @_;
+
+    my ( $id ) = $self->_result()->id();
+
+    return $id;
+}
+
+=head3 $object->_result();
+
+Returns the internal DBIC Row object
+
+=cut
+
+sub _result {
+    my ($self) = @_;
+
+    # If we don't have a dbic row at this point, we need to create an empty one
+    $self->{_result} ||=
+      Koha::Database->new()->schema()->resultset( $self->type() )->new({});
+
+    return $self->{_result};
+}
+
+=head3 $object->_columns();
+
+Returns an arrayref of the table columns
+
+=cut
+
+sub _columns {
+    my ($self) = @_;
+
+    # If we don't have a dbic row at this point, we need to create an empty one
+    $self->{_columns} ||= [ $self->_result()->result_source()->columns() ];
+
+    return $self->{_columns};
+}
+
+
+=head3 AUTOLOAD
+
+The autoload method is used only to get and set values for an objects properties.
+
+=cut
+
+sub AUTOLOAD {
+    my $self = shift;
+
+    my $method = our $AUTOLOAD;
+    $method =~ s/.*://;
+
+    my @columns = @{$self->_columns()};
+    # Using direct setter/getter like $item->barcode() or $item->barcode($barcode);
+    if ( $method ~~ @columns ) {
+        if ( @_ ) {
+            return $self->_result()->set_column( $method, @_ );
+        } else {
+            my $value = $self->_result()->get_column( $method );
+            return encode( 'UTF-8', $value );
+        }
+    }
+
+    carp "No method $method!";
+    return;
+}
+
+=head3 type
+
+This method must be defined in the child class. The value is the name of the DBIC resultset.
+For example, for borrowers, the type method will return "Borrower".
+
+=cut
+
+sub type { }
+
+sub DESTROY { }
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

t/db_dependent/Borrower.t
@@ -1,0 +1,70 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Test::More tests => 12;
+use Test::Warn;
+
+use C4::Context;
+use Koha::Database;
+
+BEGIN {
+    use_ok('Koha::Object');
+    use_ok('Koha::Borrower');
+}
+
+# Start transaction
+my $dbh = C4::Context->dbh;
+$dbh->{AutoCommit} = 0;
+$dbh->{RaiseError} = 1;
+
+my $categorycode = Koha::Database->new()->schema()->resultset('Category')->first()->categorycode();
+my $branchcode = Koha::Database->new()->schema()->resultset('Branch')->first()->branchcode();
+
+my $object = Koha::Borrower->new();
+
+is( $object->in_storage, 0, "Object is not in storage" );
+
+$object->categorycode( $categorycode );
+$object->branchcode( $branchcode );
+$object->surname("Test Surname");
+$object->store();
+
+my $borrower = Koha::Database->new()->schema()->resultset('Borrower')->find( $object->borrowernumber() );
+is( $borrower->surname(), "Test Surname", "Object found in database" );
+
+is( $object->in_storage, 1, "Object is now stored" );
+
+is( $object->is_changed(), 0, "Object is unchanged" );
+$object->surname("Test Surname 2");
+is( $object->is_changed(), 1, "Object is changed" );
+
+$object->store();
+is( $object->is_changed(), 0, "Object no longer marked as changed after being stored" );
+
+$object->set({ firstname => 'Test Firstname' });
+is( $object->is_changed(), 1, "Object is changed after Set" );
+$object->store();
+is( $object->is_changed(), 0, "Object no longer marked as changed after being stored" );
+
+$object->delete();
+$borrower = Koha::Database->new()->schema()->resultset('Borrower')->find( $object->borrowernumber() );
+ok( ! $borrower, "Object no longer found in database" );
+is( $object->in_storage, 0, "Object is not in storage" );
+
+1;