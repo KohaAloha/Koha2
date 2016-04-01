Koha-Community/Koha

commit 243b797dd11bc0ac1f475195ab765ffbf2220f8b

Bug 15285: Update common files
This patch moves the Button area all tables which does not redefine the
dom/sDom DT parameter.

Signed-off-by: Marc Véron <veron@veron.ch>

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

C4/Utils/DataTables/ColumnsSettings.pm
@@ -44,7 +44,18 @@ sub get_columns {
         $column->{cannot_be_toggled} = $c->cannot_be_toggled;
     }
 
-    return $list->{modules}{$module}{$page}{$tablename} || [];
+    my $columns = $list->{modules}{$module}{$page}{$tablename} || [];
+
+    # Assign default value if does not exist
+    $columns = [ map {
+        {
+            cannot_be_toggled => exists $_->{cannot_be_toggled} ? $_->{cannot_be_toggled} : 0,
+            is_hidden => exists $_->{is_hidden} ? $_->{is_hidden} : 0,
+            columnname => $_->{columnname},
+        }
+    } @$columns ];
+
+    return $columns;
 }
 
 sub get_modules {

koha-tmpl/intranet-tmpl/prog/en/includes/columns_settings.inc
@@ -1,13 +1,14 @@
 [% USE ColumnsSettings %]
 
-<link rel="stylesheet" type="text/css" href="[% themelang %]/css/datatables.colvis.css" />
-<script type="text/javascript" src="[% interface %]/lib/jquery/plugins/jquery.dataTables.colvis.js"></script>
+<link rel="stylesheet" type="text/css" href="[% themelang %]/css/buttons.dataTables.min.css" />
+<script type="text/javascript" src="[% interface %]/lib/jquery/plugins/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="[% interface %]/lib/jquery/plugins/buttons.colVis.min.js"></script>
 
 <script type="text/javascript">
 function KohaTable(selector, dt_parameters, columns_settings) {
     var id = 0;
     var hidden_ids = [];
-    var excluded_ids = [];
+    var included_ids = [];
     $(columns_settings).each( function() {
         var named_id = $( 'thead th[data-colname="' + this.columnname + '"]', selector ).index( 'th' );
         var used_id = dt_parameters.bKohaColumnsUseNames ? named_id : id;
@@ -16,12 +17,17 @@ function KohaTable(selector, dt_parameters, columns_settings) {
         if ( this['is_hidden'] == "1" ) {
             hidden_ids.push( used_id );
         }
-        if ( this['cannot_be_toggled'] == "1" ) {
-            excluded_ids.push( used_id );
+        if ( this['cannot_be_toggled'] == "0" ) {
+            included_ids.push( used_id );
         }
         id++;
     });
-    dt_parameters[ "oColVis" ] = { "aiExclude": excluded_ids };
+    dt_parameters[ "buttons" ] = [
+        {
+            extend: 'colvis',
+            columns: included_ids,
+        }
+    ];
     var table = $(selector).dataTable($.extend(true, {}, dataTablesDefaults, dt_parameters));
 
     $(hidden_ids).each(function(index, value) {

koha-tmpl/intranet-tmpl/prog/en/includes/datatables.inc
@@ -1,5 +1,7 @@
 [% INCLUDE 'format_price.inc' %]
 <script type="text/javascript" src="[% interface %]/lib/jquery/plugins/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="[% interface %]/lib/jquery/plugins/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="[% interface %]/lib/jquery/plugins/buttons.colVis.min.js"></script>
 <script type="text/javascript">
 //<![CDATA[
     var MSG_DT_FIRST = _("First");

koha-tmpl/intranet-tmpl/prog/en/js/datatables.js
@@ -23,7 +23,7 @@ var dataTablesDefaults = {
         "sSearch"           : window.MSG_DT_SEARCH || "Search:",
         "sZeroRecords"      : window.MSG_DT_ZERO_RECORDS || "No matching records found"
     },
-    "sDom": 'C<"top pager"ilpf>tr<"bottom pager"ip>',
+    "dom": '<"top pager"ilpfB>tr<"bottom pager"ip>',
     "aLengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, window.MSG_DT_ALL || "All"]],
     "iDisplayLength": 20
 };

KohaAloha/Koha2

commit efc64eadb30a96eb3b3390fe9f55fee00a080bda

Bug 16177: Fix tests for ColumnsSsettings.t
This failure has been introduced by
  commit 243b797dd11bc0ac1f475195ab765ffbf2220f8b
    Bug 15285: Update common files

This is because the cannot_be_modified key is not always created by
C4::Utils::DataTables::ColumnsSettings subroutines

Test plan:
  prove t/db_dependent/ColumnsSettings.t
should return green
And you can also test a table where the feature is set and a column
cannot be modified from the admin page.

NOTE: Works as described. I was pondering this potential
      solution when I found it in bugzilla.

Signed-off-by: Mark Tompsett <mtompset@hotmail.com>

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

Signed-off-by: Brendan A Gallagher <brendan@bywatersolutions.com>

C4/Utils/DataTables/ColumnsSettings.pm
@@ -50,6 +50,7 @@ sub get_columns {
     $columns = [ map {
         {
             cannot_be_toggled => exists $_->{cannot_be_toggled} ? $_->{cannot_be_toggled} : 0,
+            cannot_be_modified => exists $_->{cannot_be_modified} ? $_->{cannot_be_modified} : 0,
             is_hidden => exists $_->{is_hidden} ? $_->{is_hidden} : 0,
             columnname => $_->{columnname},
         }
@@ -69,6 +70,8 @@ sub get_modules {
         @{ $list->{modules}{ $c->module }{ $c->page }{ $c->tablename } };
         $column->{is_hidden}         = $c->is_hidden;
         $column->{cannot_be_toggled} = $c->cannot_be_toggled;
+        $column->{cannot_be_modified} = 0
+            unless exists $column->{cannot_be_modified};
     }
 
     return $list->{modules};

t/db_dependent/ColumnsSettings.t
@@ -133,26 +133,31 @@ my $modules_expected = {
                 {
                     columnname => 'symbol',
                     cannot_be_toggled  => 0,
+                    cannot_be_modified => 0,
                     is_hidden  => 0,
                 },
                 {
                     columnname => 'iso_code',
                     cannot_be_toggled  => 0,
+                    cannot_be_modified => 0,
                     is_hidden  => 0,
                 },
                 {
                     columnname => 'last_updated',
                     cannot_be_toggled  => 0,
+                    cannot_be_modified => 0,
                     is_hidden  => 0,
                 },
                 {
                     columnname => 'active',
                     cannot_be_toggled  => 0,
+                    cannot_be_modified => 0,
                     is_hidden  => 1,
                 },
                 {
                     columnname        => 'actions',
                     cannot_be_toggled => 1,
+                    cannot_be_modified => 0,
                     is_hidden  => 0,
                 },
             ]