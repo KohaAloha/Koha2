bywatersolutions/bywater-koha-devel

Bug 12461 - Add patron clubs feature
This features would add the ability to create clubs which patrons may be
enrolled in. It would be particularly useful for tracking summer reading
programs, book clubs and other such clubs.

Test Plan:
1) Apply this patch
2) Run updatedatabase.pl
3) Ensure your staff user has the new 'Patron clubs' permissions
4) Under the tools menu, click the "Patron clubs" link
5) Create a new club template
   * Here you can add fields that can be filled out at the time
     a new club is created based on the template, or a new enrollment
     is created for a given club based on the template.
6) Create a new club based on that template
7) Attempt to enroll a patron in that club
8) Create a club with email required set
9) Attempt to enroll a patron without an email address in that club
10) Create a club that is enrollable from the OPAC
11) Attempt to enroll a patron in that club
12) Attempt to cancel a club enrollment from the OPAC
13) Attempt to cancel a club enrollment from the staff interface

Followed test plan, works as expected.
Signed-off-by: Marc Véron <veron@veron.ch>

C4/Members.pm
@@ -974,20 +974,10 @@ addresses.
 
 sub GetFirstValidEmailAddress {
     my $borrowernumber = shift;
-    my $dbh = C4::Context->dbh;
-    my $sth = $dbh->prepare( "SELECT email, emailpro, B_email FROM borrowers where borrowernumber = ? ");
-    $sth->execute( $borrowernumber );
-    my $data = $sth->fetchrow_hashref;
 
-    if ($data->{'email'}) {
-       return $data->{'email'};
-    } elsif ($data->{'emailpro'}) {
-       return $data->{'emailpro'};
-    } elsif ($data->{'B_email'}) {
-       return $data->{'B_email'};
-    } else {
-       return '';
-    }
+    my $borrower = Koha::Patrons->find( $borrowernumber );
+
+    return $borrower->FirstValidEmailAddress();
 }
 
 =head2 GetNoticeEmailAddress

Koha/Clubs.pm
@@ -1,0 +1,92 @@
+package Koha::Clubs;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use Koha::Club;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::Clubs - Koha Clubs Object class
+
+This object represents a collection of clubs a patron may enroll in.
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 GetEnrollable
+
+=cut
+
+sub GetEnrollable {
+    my ( $self, $params ) = @_;
+
+    # We need to filter out all the already enrolled in clubs
+    my $borrower = $params->{borrower};
+    if ($borrower) {
+        delete( $params->{borrower} );
+        my @enrollments = $borrower->GetClubEnrollments();
+        if (@enrollments) {
+            $params->{'me.id'} = { -not_in => [ map { $_->club()->id() } @enrollments ] };
+        }
+    }
+
+    my $rs = $self->_resultset()->search( $params, { prefetch => 'club_template' } );
+
+    if (wantarray) {
+        my $class = ref($self) ? ref($self) : $self;
+
+        return $class->_wrap( $rs->all() );
+
+    }
+    else {
+        my $class = ref($self) ? ref($self) : $self;
+
+        return $class->_new_from_dbic($rs);
+    }
+}
+
+=head3 type
+
+=cut
+
+sub _type {
+    return 'Club';
+}
+
+sub object_class {
+    return 'Koha::Club';
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

clubs/patron-clubs-tab.pl
@@ -1,0 +1,55 @@
+#!/usr/bin/perl
+
+# Copyright 2013 ByWater Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth;
+use C4::Output;
+
+use Koha::Patrons;
+use Koha::Club::Enrollments;
+
+my $cgi = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "clubs/patron-clubs-tab.tt",
+        query           => $cgi,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { clubs => '*' },
+    }
+);
+
+my $borrowernumber = $cgi->param('borrowernumber');
+
+my $borrower = Koha::Patrons->find($borrowernumber);
+
+my @enrollments = $borrower->GetClubEnrollments();
+my @clubs       = $borrower->GetEnrollableClubs();
+
+$template->param(
+    enrollments    => \@enrollments,
+    clubs          => \@clubs,
+    borrowernumber => $borrowernumber
+);
+
+output_html_with_http_headers( $cgi, $cookie, $template->output );

clubs/templates-add-modify.pl
@@ -1,0 +1,150 @@
+#!/usr/bin/perl
+
+# Copyright 2013 ByWater Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth;
+use C4::Output;
+
+use Koha::DateUtils qw(dt_from_string);
+use Koha::Club::Templates;
+use Koha::Club::Template::Fields;
+use Koha::Club::Template::EnrollmentFields;
+
+use Koha::Database;
+my $schema = Koha::Database->new()->schema();
+
+my $cgi = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => 'clubs/templates-add-modify.tt',
+        query           => $cgi,
+        type            => 'intranet',
+        authnotrequired => 0,
+        flagsrequired   => { clubs => 'edit_templates' },
+    }
+);
+
+my $id = $cgi->param('id');
+
+my $club_template;
+
+if ( $cgi->param('name') ) {    # Update or create club
+    if ($id) {
+        $club_template = Koha::Club::Templates->find($id);
+        $template->param( stored => 'updated' );
+    }
+    else {
+        $club_template = Koha::Club::Template->new();
+        $template->param( stored => 'created' );
+    }
+
+    $club_template->set(
+        {
+            id          => $id                        || undef,
+            name        => $cgi->param('name')        || undef,
+            description => $cgi->param('description') || undef,
+            branchcode  => $cgi->param('branchcode')  || undef,
+            date_updated            => dt_from_string(),
+            is_email_required       => $cgi->param('is_email_required') ? 1 : 0,
+            is_enrollable_from_opac => $cgi->param('is_enrollable_from_opac')
+            ? 1
+            : 0,
+        }
+    )->store();
+
+    $id ||= $club_template->id();
+
+    # Update club creation fields
+    my @field_id                        = $cgi->param('club_template_field_id');
+    my @field_name                      = $cgi->param('club_template_field_name');
+    my @field_description               = $cgi->param('club_template_field_description');
+    my @field_authorised_value_category = $cgi->param('club_template_field_authorised_value_category');
+
+    my @field_delete = $cgi->param('club_template_field_delete');
+
+    for ( my $i = 0 ; $i < @field_id ; $i++ ) {
+        my $field_id                        = $field_id[$i];
+        my $field_name                      = $field_name[$i];
+        my $field_description               = $field_description[$i];
+        my $field_authorised_value_category = $field_authorised_value_category[$i];
+
+        my $field =
+          $field_id
+          ? Koha::Club::Template::Fields->find($field_id)
+          : Koha::Club::Template::Field->new();
+
+        if ( grep( /^$field_id$/, @field_delete ) ) {
+            $field->delete();
+        }
+        else {
+            $field->set(
+                {
+                    club_template_id          => $id,
+                    name                      => $field_name,
+                    description               => $field_description,
+                    authorised_value_category => $field_authorised_value_category,
+                }
+            )->store();
+        }
+    }
+
+    # Update club enrollment fields
+    @field_id                        = $cgi->param('club_template_enrollment_field_id');
+    @field_name                      = $cgi->param('club_template_enrollment_field_name');
+    @field_description               = $cgi->param('club_template_enrollment_field_description');
+    @field_authorised_value_category = $cgi->param('club_template_enrollment_field_authorised_value_category');
+
+    @field_delete = $cgi->param('club_template_enrollment_field_delete');
+
+    for ( my $i = 0 ; $i < @field_id ; $i++ ) {
+        my $field_id                        = $field_id[$i];
+        my $field_name                      = $field_name[$i];
+        my $field_description               = $field_description[$i];
+        my $field_authorised_value_category = $field_authorised_value_category[$i];
+
+        my $field =
+          $field_id
+          ? Koha::Club::Template::EnrollmentFields->find($field_id)
+          : Koha::Club::Template::EnrollmentField->new();
+
+        if ( grep( /^$field_id$/, @field_delete ) ) {
+            $field->delete();
+        }
+        else {
+            $field->set(
+                {
+                    id                        => $field_id,
+                    club_template_id          => $id,
+                    name                      => $field_name,
+                    description               => $field_description,
+                    authorised_value_category => $field_authorised_value_category,
+                }
+            )->store();
+        }
+    }
+}
+
+$club_template ||= Koha::Club::Templates->find($id);
+$template->param( club_template => $club_template );
+
+output_html_with_http_headers( $cgi, $cookie, $template->output );

KohaAloha/Koha2

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

C4/Members.pm
@@ -989,20 +989,10 @@ addresses.
 
 sub GetFirstValidEmailAddress {
     my $borrowernumber = shift;
-    my $dbh = C4::Context->dbh;
-    my $sth = $dbh->prepare( "SELECT email, emailpro, B_email FROM borrowers where borrowernumber = ? ");
-    $sth->execute( $borrowernumber );
-    my $data = $sth->fetchrow_hashref;
 
-    if ($data->{'email'}) {
-       return $data->{'email'};
-    } elsif ($data->{'emailpro'}) {
-       return $data->{'emailpro'};
-    } elsif ($data->{'B_email'}) {
-       return $data->{'B_email'};
-    } else {
-       return '';
-    }
+    my $borrower = Koha::Patrons->find( $borrowernumber );
+
+    return $borrower->first_valid_email_address();
 }
 
 =head2 GetNoticeEmailAddress

Koha/Clubs.pm
@@ -1,0 +1,92 @@
+package Koha::Clubs;
+
+# Copyright ByWater Solutions 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use Koha::Club;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::Clubs - Koha Clubs Object class
+
+This object represents a collection of clubs a patron may enroll in.
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 get_enrollable
+
+=cut
+
+sub get_enrollable {
+    my ( $self, $params ) = @_;
+
+    # We need to filter out all the already enrolled in clubs
+    my $borrower = $params->{borrower};
+    if ($borrower) {
+        delete( $params->{borrower} );
+        my @enrollments = $borrower->get_club_enrollments();
+        if (@enrollments) {
+            $params->{'me.id'} = { -not_in => [ map { $_->club()->id() } @enrollments ] };
+        }
+    }
+
+    my $rs = $self->_resultset()->search( $params, { prefetch => 'club_template' } );
+
+    if (wantarray) {
+        my $class = ref($self) ? ref($self) : $self;
+
+        return $class->_wrap( $rs->all() );
+
+    }
+    else {
+        my $class = ref($self) ? ref($self) : $self;
+
+        return $class->_new_from_dbic($rs);
+    }
+}
+
+=head3 type
+
+=cut
+
+sub _type {
+    return 'Club';
+}
+
+sub object_class {
+    return 'Koha::Club';
+}
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+=cut
+
+1;

clubs/patron-clubs-tab.pl
@@ -1,0 +1,55 @@
+#!/usr/bin/perl
+
+# Copyright 2013 ByWater Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth;
+use C4::Output;
+
+use Koha::Patrons;
+use Koha::Club::Enrollments;
+
+my $cgi = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "clubs/patron-clubs-tab.tt",
+        query           => $cgi,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { clubs => '*' },
+    }
+);
+
+my $patronnumber = $cgi->param('borrowernumber');
+
+my $patron = Koha::Patrons->find($patronnumber);
+
+my @enrollments = $patron->get_club_enrollments();
+my @clubs       = $patron->get_enrollable_clubs();
+
+$template->param(
+    enrollments    => \@enrollments,
+    clubs          => \@clubs,
+    borrowernumber => $patronnumber
+);
+
+output_html_with_http_headers( $cgi, $cookie, $template->output );

clubs/templates-add-modify.pl
@@ -1,0 +1,152 @@
+#!/usr/bin/perl
+
+# Copyright 2013 ByWater Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth;
+use C4::Output;
+
+use Koha::DateUtils qw(dt_from_string);
+use Koha::Club::Templates;
+use Koha::Club::Template::Fields;
+use Koha::Club::Template::EnrollmentFields;
+
+use Koha::Database;
+my $schema = Koha::Database->new()->schema();
+
+my $cgi = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => 'clubs/templates-add-modify.tt',
+        query           => $cgi,
+        type            => 'intranet',
+        authnotrequired => 0,
+        flagsrequired   => { clubs => 'edit_templates' },
+    }
+);
+
+my $id = $cgi->param('id');
+
+my $club_template;
+my $stored;
+
+if ( $cgi->param('name') ) {    # Update or create club
+    if ($id) {
+        $club_template = Koha::Club::Templates->find($id);
+        $stored        = 'updated';
+    }
+    else {
+        $club_template = Koha::Club::Template->new();
+        $stored = 'created';
+    }
+
+    $club_template->set(
+        {
+            id          => $id                        || undef,
+            name        => $cgi->param('name')        || undef,
+            description => $cgi->param('description') || undef,
+            branchcode  => $cgi->param('branchcode')  || undef,
+            date_updated            => dt_from_string(),
+            is_email_required       => $cgi->param('is_email_required') ? 1 : 0,
+            is_enrollable_from_opac => $cgi->param('is_enrollable_from_opac') ? 1 : 0,
+        }
+    )->store();
+
+    $id ||= $club_template->id();
+
+    # Update club creation fields
+    my @field_id                        = $cgi->multi_param('club_template_field_id');
+    my @field_name                      = $cgi->multi_param('club_template_field_name');
+    my @field_description               = $cgi->multi_param('club_template_field_description');
+    my @field_authorised_value_category = $cgi->multi_param('club_template_field_authorised_value_category');
+
+    my @field_delete = $cgi->multi_param('club_template_field_delete');
+
+    for ( my $i = 0 ; $i < @field_id ; $i++ ) {
+        my $field_id                        = $field_id[$i];
+        my $field_name                      = $field_name[$i];
+        my $field_description               = $field_description[$i];
+        my $field_authorised_value_category = $field_authorised_value_category[$i];
+
+        my $field =
+          $field_id
+          ? Koha::Club::Template::Fields->find($field_id)
+          : Koha::Club::Template::Field->new();
+
+        if ( grep( /^$field_id$/, @field_delete ) ) {
+            $field->delete();
+        }
+        else {
+            $field->set(
+                {
+                    club_template_id          => $id,
+                    name                      => $field_name,
+                    description               => $field_description,
+                    authorised_value_category => $field_authorised_value_category,
+                }
+            )->store();
+        }
+    }
+
+    # Update club enrollment fields
+    @field_id                        = $cgi->multi_param('club_template_enrollment_field_id');
+    @field_name                      = $cgi->multi_param('club_template_enrollment_field_name');
+    @field_description               = $cgi->multi_param('club_template_enrollment_field_description');
+    @field_authorised_value_category = $cgi->multi_param('club_template_enrollment_field_authorised_value_category');
+
+    @field_delete = $cgi->multi_param('club_template_enrollment_field_delete');
+
+    for ( my $i = 0 ; $i < @field_id ; $i++ ) {
+        my $field_id                        = $field_id[$i];
+        my $field_name                      = $field_name[$i];
+        my $field_description               = $field_description[$i];
+        my $field_authorised_value_category = $field_authorised_value_category[$i];
+
+        my $field =
+          $field_id
+          ? Koha::Club::Template::EnrollmentFields->find($field_id)
+          : Koha::Club::Template::EnrollmentField->new();
+
+        if ( grep( /^$field_id$/, @field_delete ) ) {
+            $field->delete();
+        }
+        else {
+            $field->set(
+                {
+                    id                        => $field_id,
+                    club_template_id          => $id,
+                    name                      => $field_name,
+                    description               => $field_description,
+                    authorised_value_category => $field_authorised_value_category,
+                }
+            )->store();
+        }
+    }
+
+    print $cgi->redirect("/cgi-bin/koha/clubs/clubs.pl?stored=$stored&club_template_id=$id");
+    exit;
+}
+
+$club_template ||= Koha::Club::Templates->find($id);
+$template->param( club_template => $club_template );
+
+output_html_with_http_headers( $cgi, $cookie, $template->output );