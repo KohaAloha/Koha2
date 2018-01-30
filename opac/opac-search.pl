Koha-Community/Koha

commit 091d6c513bcbee224ff06477e79be48cea7fe825

Bug 17843: Replace C4::Koha::getitemtypeinfo with Koha::ItemTypes
The C4::Koha::getitemtypeinfo subroutine did the almost same job as
GetItemTypes. On top of that it returned the imageurl value processed by
C4::Koha::getitemtypeimagelocation.
This value is only used from the 2 [opac-]shelves.pl scripts. Then it's
better not retrieve it only when we need it.

Test plan:
Play with the different scripts touched by this patch and focus on item
types. The same description as prior to this patch must be displayed.
Note that sometimes it is not the translated description which is
displayed, but that should be fixed on another bug report. Indeed we do
not expect this patch to change any behaviors.

Signed-off-by: Lari Taskula <lari.taskula@jns.fi>

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

opac/opac-search.pl
@@ -52,6 +52,7 @@ use C4::Tags qw(get_tags);
 use C4::SocialData;
 use C4::External::OverDrive;
 
+use Koha::ItemTypes;
 use Koha::LibraryCategories;
 use Koha::Ratings;
 use Koha::Virtualshelves;
@@ -225,7 +226,7 @@ my $itemtypes = GetItemTypesCategorized;
 # add translated_description to itemtypes
 foreach my $itemtype ( keys %{$itemtypes} ) {
     # Itemtypes search categories don't have (yet) translated descriptions, they are auth values (and could still have no descriptions too BZ 18400)
-    my $translated_description = getitemtypeinfo( $itemtype, 'opac' )->{translated_description};
+    my $translated_description = Koha::ItemTypes->find( $itemtype )->translated_description;
     $itemtypes->{$itemtype}->{translated_description} = $translated_description || $itemtypes->{$itemtype}->{description} || q{};
 }
 

C4/Biblio.pm
@@ -44,6 +44,7 @@ use Koha::Acquisition::Currencies;
 use Koha::Biblio::Metadata;
 use Koha::Biblio::Metadatas;
 use Koha::Holds;
+use Koha::ItemTypes;
 use Koha::SearchEngine;
 use Koha::Libraries;
 
@@ -1629,7 +1630,8 @@ sub GetAuthorisedValueDesc {
 
         #---- itemtypes
         if ( $tagslib->{$tag}->{$subfield}->{'authorised_value'} eq "itemtypes" ) {
-            return getitemtypeinfo($value)->{translated_description};
+            my $itemtype = Koha::ItemTypes->find( $value );
+            return $itemtype ? $itemtype->translated_description : q||;
         }
 
         #---- "true" authorized value

Koha/Template/Plugin/ItemTypes.pm
@@ -22,15 +22,11 @@ use Modern::Perl;
 use Template::Plugin;
 use base qw( Template::Plugin );
 
-use C4::Koha;
 use Koha::ItemTypes;
 
 sub GetDescription {
     my ( $self, $itemtype ) = @_;
-
-    $itemtype = C4::Koha::getitemtypeinfo( $itemtype );
-    return $itemtype->{translated_description};
-
+    return Koha::ItemTypes->find( $itemtype )->translated_description;
 }
 
 sub Get {

acqui/orderreceive.pl
@@ -75,6 +75,7 @@ use C4::Koha;
 
 use Koha::Acquisition::Booksellers;
 use Koha::DateUtils qw( dt_from_string );
+use Koha::ItemTypes;
 
 my $input      = new CGI;
 
@@ -144,8 +145,8 @@ if ($AcqCreateItem eq 'receiving') {
         $descriptions = Koha::AuthorisedValues->get_description_by_koha_field({frameworkcode => $fw, kohafield => 'items.materials', authorised_value => $item->{materials} });
         $item->{materials} = $descriptions->{lib} // '';
 
-        my $itemtype = getitemtypeinfo($item->{itype});
-        $item->{itemtype} = $itemtype->{description};
+        my $itemtype = Koha::ItemsTypes->find( $item->{itype} );
+        $item->{itemtype} = $itemtype->description; # FIXME Should not it be translated_description?
         push @items, $item;
     }
     $template->param(items => \@items);

catalogue/getitem-ajax.pl
@@ -29,6 +29,7 @@ use C4::Output;
 use Koha::Libraries;
 
 use Koha::AuthorisedValues;
+use Koha::ItemTypes;
 
 my $cgi = new CGI;
 
@@ -71,8 +72,8 @@ if($itemnumber) {
     $descriptions = Koha::AuthorisedValues->get_description_by_koha_field({ frameworkcode => $fw, kohafield => 'items.materials', authorised_value => $item->{materials} });
     $item->{materials} = $descriptions->{lib} // '';
 
-    my $itemtype = getitemtypeinfo($item->{itype});
-    $item->{itemtype} = $itemtype->{description};
+    my $itemtype = Koha::ItemTypes->find( $item->{itype} );
+    $item->{itemtype} = $itemtype->description; # FIXME Should not it be translated_description?
 }
 
 my $json_text = to_json( $item, { utf8 => 1 } );

circ/transferstoreceive.pl
@@ -36,6 +36,7 @@ use Date::Calc qw(
 use C4::Koha;
 use C4::Reserves;
 use Koha::Items;
+use Koha::ItemTypes;
 use Koha::Libraries;
 use Koha::DateUtils;
 use Koha::BiblioFrameworks;
@@ -89,10 +90,10 @@ while ( my $library = $libraries->next ) {
 				$getransf{'diff'} = $diff;
             }
             my $gettitle     = GetBiblioFromItemNumber( $num->{'itemnumber'} );
-            my $itemtypeinfo = getitemtypeinfo( (C4::Context->preference('item-level_itypes')) ? $gettitle->{'itype'} : $gettitle->{'itemtype'} );
+            my $itemtype = Koha::ItemTypes->find( (C4::Context->preference('item-level_itypes')) ? $gettitle->{'itype'} : $gettitle->{'itemtype'} );
 
             $getransf{'datetransfer'} = $num->{'datesent'};
-            $getransf{'itemtype'} = $itemtypeinfo ->{'description'};
+            $getransf{'itemtype'} = $itemtype->description; # FIXME Should not it be translated_description?
 			foreach (qw(title author biblionumber itemnumber barcode homebranch holdingbranch itemcallnumber)) {
             	$getransf{$_} = $gettitle->{$_};
 			}

circ/waitingreserves.pl
@@ -37,6 +37,7 @@ use C4::Reserves;
 use C4::Koha;
 use Koha::DateUtils;
 use Koha::BiblioFrameworks;
+use Koha::ItemTypes;
 
 my $input = new CGI;
 
@@ -104,12 +105,12 @@ foreach my $num (@getreserves) {
     # fix up item type for display
     $gettitle->{'itemtype'} = C4::Context->preference('item-level_itypes') ? $gettitle->{'itype'} : $gettitle->{'itemtype'};
     my $getborrower = GetMember(borrowernumber => $num->{'borrowernumber'});
-    my $itemtypeinfo = getitemtypeinfo( $gettitle->{'itemtype'} );  # using the fixed up itype/itemtype
+    my $itemtype = Koha::ItemTypes->find( $gettitle->{'itemtype'} );  # using the fixed up itype/itemtype
     $getreserv{'waitingdate'} = $num->{'waitingdate'};
     my ( $expire_year, $expire_month, $expire_day ) = split (/-/, $num->{'expirationdate'});
     my $calcDate = Date_to_Days( $expire_year, $expire_month, $expire_day );
 
-    $getreserv{'itemtype'}       = $itemtypeinfo->{'description'};
+    $getreserv{'itemtype'}       = $itemtype->description; # FIXME Should not it be translated_description?
     $getreserv{'title'}          = $gettitle->{'title'};
     $getreserv{'subtitle'}       = GetRecordValue('subtitle', GetMarcBiblio($gettitle->{'biblionumber'}), GetFrameworkCode($gettitle->{'biblionumber'}));
     $getreserv{'biblionumber'}   = $gettitle->{'biblionumber'};

members/summary-print.pl
@@ -22,11 +22,11 @@ use CGI;
 use C4::Auth;
 use C4::Output;
 use C4::Members;
-use C4::Koha qw( getitemtypeinfo );
 use C4::Circulation qw( GetIssuingCharges );
 use C4::Reserves;
 use C4::Items;
 use Koha::Holds;
+use Koha::ItemTypes;
 
 my $input          = CGI->new;
 my $borrowernumber = $input->param('borrowernumber');
@@ -94,8 +94,8 @@ sub build_issue_data {
         my ( $charge, $itemtype ) =
           GetIssuingCharges( $issue->{itemnumber}, $borrowernumber );
 
-        my $itemtypeinfo = getitemtypeinfo($itemtype);
-        $row{'itemtype_description'} = $itemtypeinfo->{description};
+        my $itemtype = Koha::ItemTypes->find( $itemtype );
+        $row{'itemtype_description'} = $itemtype->description; #FIXME Should not it be translated_description
 
         $row{'charge'} = sprintf( "%.2f", $charge );
 

KohaAloha/Koha2

commit 4f0a9531a0b4939f988b9303970742be1d3cdc12

Bug 19975: Fix search by tags at the OPAC
This bug has certainly be caused by
  commit 091d6c513bcbee224ff06477e79be48cea7fe825
    Bug 17843: Replace C4::Koha::getitemtypeinfo with Koha::ItemTypes

The reason is quite simple, in Koha::ItemType->translated_description
(called in opac-search.pl l.229) there is an eval to know if we have
access to the translated description of the item type, to avoid
retrieving it again.

The evaluation of $@ later in opac-search.pl is supposed to test the 2
eval made few lines before (a "normal" search, without tags), but
$@ contains the error message from the *last* eval command.
So we are raising an error that have been correctly handled in
Koha::ItemType.

Test plan:
At the OPAC, click Tag cloud, then click any of the tags

=> Without the patch you get
Koha::ItemType::get_column generated this error: DBIx::Class::Row::get_column(): No such column 'translated_description' on Koha::Schema::Result::Itemtype at /usr/share/koha/lib/Koha/Object.pm line 307

=> With the patch applied the page is correctly displayed

Signed-off-by: Claire Gravely <claire.gravely@bsz-bw.de>

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

opac/opac-search.pl
@@ -611,7 +611,7 @@ if ($tag) {
 }
 
 # use Data::Dumper; print STDERR "-" x 25, "\n", Dumper($results_hashref);
-if ($@ || $error) {
+if (not $tag and ( $@ || $error)) {
     $template->param(query_error => $error.$@);
     output_html_with_http_headers $cgi, $cookie, $template->output;
     exit;