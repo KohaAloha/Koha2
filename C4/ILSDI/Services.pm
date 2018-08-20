Koha-Community/Koha

commit cb336e633bf7e0cb25051a8ed8157af8486a58d7

Bug 18255: Koha::Biblio - Replace GetBiblioItemByBiblioNumber with Koha::Biblio->biblioitem
The subroutine GetBiblioItemByBiblioNumber considers that we have a 1-N
relation between biblio and biblioitems, which is wrong (it's 1-1).
So the calls can be replaced with Koha::biblio->biblioitem, it will ease
the read of the code.

Test plan:
1. Use the ILSDI service to display info of a bibliographic record,
biblioitems fields must be displayed
2. Search for items, biblioitems info must be displayed as well in the
result table

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

C4/ILSDI/Services.pm
@@ -208,9 +208,13 @@ sub GetRecords {
     foreach my $biblionumber ( split( / /, $cgi->param('id') ) ) {
 
         # Get the biblioitem from the biblionumber
-        my $biblioitem = ( GetBiblioItemByBiblioNumber( $biblionumber, undef ) )[0];
-        if ( not $biblioitem->{'biblionumber'} ) {
+        my $biblio = Koha::Biblios->find( $biblionumber );
+        my $biblioitem = $biblio->biblioitem;
+        if ( $biblioitem ) {
+            $biblioitem = $biblioitem->unblessed;
+        } else {
             $biblioitem->{code} = "RecordNotFound";
+            # FIXME We should not need to process something else; next?
         }
 
         my $embed_items = 1;
@@ -223,7 +227,6 @@ sub GetRecords {
 
         # Get most of the needed data
         my $biblioitemnumber = $biblioitem->{'biblioitemnumber'};
-        my $biblio = Koha::Biblios->find( $biblionumber );
         my $holds  = $biblio->current_holds->unblessed;
         my $issues           = GetBiblioIssues($biblionumber);
         my $items            = GetItemsByBiblioitemnumber($biblioitemnumber);

C4/Biblio.pm
@@ -69,7 +69,6 @@ BEGIN {
       GetMarcBiblio
       GetBiblioItemData
       GetBiblioItemInfosOf
-      GetBiblioItemByBiblioNumber
 
       &GetRecordValue
 
@@ -749,29 +748,6 @@ sub GetBiblioItemData {
     $sth->finish;
     return ($data);
 }    # sub &GetBiblioItemData
-
-=head2 GetBiblioItemByBiblioNumber
-
-NOTE : This function has been copy/paste from C4/Biblio.pm from head before zebra integration.
-
-=cut
-
-sub GetBiblioItemByBiblioNumber {
-    my ($biblionumber) = @_;
-    my $dbh            = C4::Context->dbh;
-    my $sth            = $dbh->prepare("Select * FROM biblioitems WHERE biblionumber = ?");
-    my $count          = 0;
-    my @results;
-
-    $sth->execute($biblionumber);
-
-    while ( my $data = $sth->fetchrow_hashref ) {
-        push @results, $data;
-    }
-
-    $sth->finish;
-    return @results;
-}
 
 =head2 GetISBDView 
 

catalogue/itemsearch.pl
@@ -233,8 +233,9 @@ if (scalar keys %params > 0) {
         }
 
         foreach my $item (@$results) {
-            $item->{biblio} = Koha::Biblios->find( $item->{biblionumber} );
-            ($item->{biblioitem}) = GetBiblioItemByBiblioNumber($item->{biblionumber});
+            my $biblio = Koha::Biblios->find( $item->{biblionumber} );
+            $item->{biblio} = $biblio;
+            $item->{biblioitem} = $biblio->biblioitem->unblessed;
             $item->{status} = $notforloan_map->{$item->{notforloan}};
             if (defined $item->{location}) {
                 $item->{location} = $location_map->{$item->{location}};

cataloguing/merge.pl
@@ -159,8 +159,6 @@ if ($merge) {
 
     # Moving orders (orders linked to items of frombiblio have already been moved by MoveItemFromBiblio)
     my @allorders = GetOrdersByBiblionumber($biblionumber);
-    my @tobiblioitem = GetBiblioItemByBiblioNumber ($ref_biblionumber);
-    my $tobiblioitem_biblioitemnumber = $tobiblioitem [0]-> {biblioitemnumber };
     foreach my $myorder (@allorders) {
         $myorder->{'biblionumber'} = $ref_biblionumber;
         ModOrder ($myorder);

serials/routing-preview.pl
@@ -66,7 +66,6 @@ my $library;
 if($ok){
     # get biblio information....
     my $biblionumber = $subs->{'bibnum'};
-    my ($count2,@bibitems) = GetBiblioItemByBiblioNumber($biblionumber);
     my @itemresults = GetItemsInfo( $biblionumber );
     my $branch = @itemresults ? $itemresults[0]->{'holdingbranch'} : $subs->{branchcode};
     $library = Koha::Libraries->find($branch);
@@ -95,7 +94,7 @@ if($ok){
                     branchcode     => $branch
                 });
             } else {
-                AddReserve($branch,$routing->{borrowernumber},$biblionumber,\@bibitems,$routing->{ranking}, undef, undef, $notes,$title);
+                AddReserve($branch,$routing->{borrowernumber},$biblionumber,undef,$routing->{ranking}, undef, undef, $notes,$title);
         }
     }
 	}

KohaAloha/Koha2

commit 72accd6147675682b170ab21edd174d362db44a7

Bug 21203: ILS-DI - Make GetRecords handle non-existent records
Caused by
  commit cb336e633bf7e0cb25051a8ed8157af8486a58d7
  Bug 18255: Koha::Biblio - Replace GetBiblioItemByBiblioNumber with Koha::Biblio->biblioitem

Can't call method "biblioitem" on an undefined value at /home/vagrant/kohaclone/C4/ILSDI/Services.pm line 212

Test plan:
- Enable ilsdi
- hit /cgi-bin/koha/ilsdi.pl?service=GetRecords&id=X
With X an existing biblionumber and a non existing one

Both requests must succeed

Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

Signed-off-by: Josef Moravec <josef.moravec@gmail.com>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

C4/ILSDI/Services.pm
@@ -209,13 +209,12 @@ sub GetRecords {
 
         # Get the biblioitem from the biblionumber
         my $biblio = Koha::Biblios->find( $biblionumber );
-        my $biblioitem = $biblio->biblioitem;
-        if ( $biblioitem ) {
-            $biblioitem = $biblioitem->unblessed;
-        } else {
-            $biblioitem->{code} = "RecordNotFound";
-            # FIXME We should not need to process something else; next?
+        unless ( $biblio ) {
+            push @records, { code => "RecordNotFound" };
+            next;
         }
+
+        my $biblioitem = $biblio->biblioitem->unblessed;
 
         my $embed_items = 1;
         my $record = GetMarcBiblio({