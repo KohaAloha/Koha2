mrenvoize/Koha

Bug 11897: Add Stock Rotation atomic update schema.
* installer/data/mysql/atomicupdate/stockrot_tables.sql: New file.
* installer/data/mysql/kohastructure.sql (stockrotationrotas)
  (stockrotationstages, stockrotationitems): New tables.
* installer/data/mysql/sysprefs.sql: Add Stockrotation sysprefs.
* installer/data/mysql/userflags.sql: Add Stockrotaiton userflag.
* installer/data/mysql/userpermissions.sql: Add Stockrotation
  userpermissions.
* koha-tmpl/intranet-tmpl/prog/en/modules/admin/preferences/circulation.pref:
  Add Stockrotation menu.

Signed-off-by: Kathleen Milne <kathleen.milne@cne-siar.gov.uk>

installer/data/mysql/kohastructure.sql
@@ -4144,6 +4144,40 @@ CREATE TABLE illrequests (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Table structure for table `stockrotationrotas`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationrotas (
+    rota_id int(11) auto_increment,          -- Stockrotation rota ID
+    title varchar(100) NOT NULL,            -- Title for this rota
+    description text NOT NULL default '',   -- Description for this rota
+    cyclical tinyint(1) NOT NULL default 0, -- Should items on this rota keep cycling?
+    active tinyint(1) NOT NULL default 0,   -- Is this rota currently active?
+    PRIMARY KEY (`rota_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `stockrotationstages`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationstages (
+    stage_id int(11) auto_increment,     -- Unique stage ID
+    position int(11) NOT NULL,           -- The position of this stage within its rota
+    rota_id int(11) NOT NULL,            -- The rota this stage belongs to
+    branchcode_id varchar(10) NOT NULL,  -- Branch this stage relates to
+    duration int(11) NOT NULL default 4, -- The number of days items shoud occupy this stage
+    PRIMARY KEY (`stage_id`),
+    CONSTRAINT `stockrotationstages_rifk`
+      FOREIGN KEY (`rota_id`)
+      REFERENCES `stockrotationrotas` (`rota_id`)
+      ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `stockrotationstages_bifk`
+      FOREIGN KEY (`branchcode_id`)
+      REFERENCES `branches` (`branchcode`)
+      ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
 -- Table structure for table `illrequestattributes`
 --
 
@@ -4192,6 +4226,26 @@ CREATE TABLE `oauth_access_tokens` (
     `expires`      INT NOT NULL,          -- expiration time in seconds
     PRIMARY KEY (`access_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `stockrotationitems`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationitems (
+    itemnumber_id int(11) NOT NULL,         -- Itemnumber to link to a stage & rota
+    stage_id int(11) NOT NULL,              -- stage ID to link the item to
+    indemand tinyint(1) NOT NULL default 0, -- Should this item be skipped for rotation?
+    fresh tinyint(1) NOT NULL default 0,    -- Flag showing item is only just added to rota
+    PRIMARY KEY (itemnumber_id),
+    CONSTRAINT `stockrotationitems_iifk`
+      FOREIGN KEY (`itemnumber_id`)
+      REFERENCES `items` (`itemnumber`)
+      ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `stockrotationitems_sifk`
+      FOREIGN KEY (`stage_id`)
+      REFERENCES `stockrotationstages` (`stage_id`)
+      ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;

installer/data/mysql/userpermissions.sql
@@ -86,4 +86,6 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (21, 'enroll', 'Enroll patrons in clubs'),
    (23, 'self_checkin_module', 'Log into the self check-in module'),
    (23, 'self_checkout_module', 'Perform self checkout at the OPAC. It should be used for the patron matching the AutoSelfCheckID')
+   (24, 'can_edit_rotas', 'Create, edit and delete rotas'),
+   (24, 'can_add_items_rotas', 'Add and remove items from rotas')
 ;

KohaAloha/Koha2

Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

installer/data/mysql/kohastructure.sql
@@ -4197,7 +4197,7 @@ CREATE TABLE library_groups (
     description MEDIUMTEXT NULL DEFAULT NULL,    -- Longer explanation of the group, if necessary
     ft_hide_patron_info tinyint(1) NOT NULL DEFAULT 0, -- Turn on the feature "Hide patron's info" for this group
     ft_search_groups_opac tinyint(1) NOT NULL DEFAULT 0, -- Use this group for staff side search groups
-    ft_search_groups_staff tinyint(1) NOT NULL DEFAULT 0, -- Use this group for opac side search groups
+     ft_search_groups_staff tinyint(1) NOT NULL DEFAULT 0, -- Use this group for opac side search groups
     created_on TIMESTAMP NULL,             -- Date and time of creation
     updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Date and time of last
     PRIMARY KEY id ( id ),
@@ -4236,6 +4236,60 @@ CREATE TABLE `circulation_rules` (
   CONSTRAINT `circ_rules_ibfk_3` FOREIGN KEY (`itemtype`) REFERENCES `itemtypes` (`itemtype`) ON DELETE CASCADE ON UPDATE CASCADE,
   KEY `rule_name` (`rule_name`),
   UNIQUE (`branchcode`,`categorycode`,`itemtype`,`rule_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `stockrotationrotas`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationrotas (
+    rota_id int(11) auto_increment,          -- Stockrotation rota ID
+    title varchar(100) NOT NULL,            -- Title for this rota
+    description text NOT NULL default '',   -- Description for this rota
+    cyclical tinyint(1) NOT NULL default 0, -- Should items on this rota keep cycling?
+    active tinyint(1) NOT NULL default 0,   -- Is this rota currently active?
+    PRIMARY KEY (`rota_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `stockrotationstages`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationstages (
+    stage_id int(11) auto_increment,     -- Unique stage ID
+    position int(11) NOT NULL,           -- The position of this stage within its rota
+    rota_id int(11) NOT NULL,            -- The rota this stage belongs to
+    branchcode_id varchar(10) NOT NULL,  -- Branch this stage relates to
+    duration int(11) NOT NULL default 4, -- The number of days items shoud occupy this stage
+    PRIMARY KEY (`stage_id`),
+    CONSTRAINT `stockrotationstages_rifk`
+      FOREIGN KEY (`rota_id`)
+      REFERENCES `stockrotationrotas` (`rota_id`)
+      ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `stockrotationstages_bifk`
+      FOREIGN KEY (`branchcode_id`)
+      REFERENCES `branches` (`branchcode`)
+      ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `stockrotationitems`
+--
+
+CREATE TABLE IF NOT EXISTS stockrotationitems (
+    itemnumber_id int(11) NOT NULL,         -- Itemnumber to link to a stage & rota
+    stage_id int(11) NOT NULL,              -- stage ID to link the item to
+    indemand tinyint(1) NOT NULL default 0, -- Should this item be skipped for rotation?
+    fresh tinyint(1) NOT NULL default 0,    -- Flag showing item is only just added to rota
+    PRIMARY KEY (itemnumber_id),
+    CONSTRAINT `stockrotationitems_iifk`
+      FOREIGN KEY (`itemnumber_id`)
+      REFERENCES `items` (`itemnumber`)
+      ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `stockrotationitems_sifk`
+      FOREIGN KEY (`stage_id`)
+      REFERENCES `stockrotationstages` (`stage_id`)
+      ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;

installer/data/mysql/userpermissions.sql
@@ -90,4 +90,6 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (21, 'enroll', 'Enroll patrons in clubs'),
    (23, 'self_checkin_module', 'Log into the self check-in module'),
    (23, 'self_checkout_module', 'Perform self checkout at the OPAC. It should be used for the patron matching the AutoSelfCheckID')
+   (24, 'manage_rotas', 'Create, edit and delete rotas'),
+   (24, 'manage_rota_items', 'Add and remove items from rotas')
 ;