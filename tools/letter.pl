Koha-Community/Koha

commit 0cffb0f7589aa52d65bda9a8a4934790a141f3a0

Bug 21621: Remove incorrect GROUP BY from tools/letter.pl
Fix:
  'koha_kohadev.letter.name' isn't in GROUP BY

DBD::mysql::db selectall_arrayref failed: 'koha_kohadev.letter.name' isn't in GROUP BY [for Statement "SELECT branchcode, module, code, name, branchname
            FROM letter
            LEFT OUTER JOIN branches USING (branchcode)
     GROUP BY branchcode,module,code ORDER BY module, code, branchcode"] at /home/vagrant/kohaclone/tools/letter.pl line 383.

Test plan:
- Copy some default notice templates for specific libraries
- List the different notice templates
- Confirm that notices appear only one, with "all libraries" you will see all

Signed-off-by: Pierre-Marc Thibault <pierre-marc.thibault@inLibro.com>

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

tools/letter.pl
@@ -377,7 +377,6 @@ sub retrieve_letters {
     }
 
     $sql .= " WHERE ".join(" AND ", @where) if @where;
-    $sql .= " GROUP BY branchcode,module,code";
     $sql .= " ORDER BY module, code, branchcode";
 
     return $dbh->selectall_arrayref($sql, { Slice => {} }, @args);

KohaAloha/Koha2

commit 2248f415b846c9f0bbab6cedf6b532ac8bdf812c

Bug 22002: (bug 21621 follow-up) Re-add GROUP BY in letter.pl and fix it
Caused by
  commit 0cffb0f7589aa52d65bda9a8a4934790a141f3a0
  Bug 21621: Remove incorrect GROUP BY from tools/letter.pl
The test plan was not complete enough.

Test plan:
1) Go to Home › Tools › Notices & slips
2) Choose a letter to edit, e.g. PREDUE. Note that there is only a
single row.
3) Click the edit button on the notice.
4) Expand the 'Print' section. Put text into the Message subject and
Message body text areas.
5) Click save, this will take you back to the 'Notices & slips' page.
6) Clone PREDUE for a specific library
7) The 'all libraries' option must list the templates for all libraries,
but without duplicate
8) Select a library in the dropdown list: only the templates for the
given library is listed, without duplicate

Signed-off-by: Barton Chittenden <barton@bywatersolutions.com>

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

koha-tmpl/intranet-tmpl/prog/en/modules/tools/letter.tt
@@ -93,7 +93,7 @@
               [% FOREACH lette IN letter %]
                 [% can_edit = lette.branchcode || !independant_branch %]
                 <tr>
-                  <td>[% IF lette.branchname %][% lette.branchname | html %][% ELSE %](All libraries)[% END %]</td>
+                  <td>X[%lette.branchcode%]X[% IF lette.branchname%][% lette.branchname | html %][% ELSE %](All libraries)[% END %]</td>
                   <td>
                       [% SWITCH lette.module %]
                           [% CASE 'acquisition' %]<span>Acquisition</span>

tools/letter.pl
@@ -377,6 +377,8 @@ sub retrieve_letters {
     }
 
     $sql .= " WHERE ".join(" AND ", @where) if @where;
+    $sql .= " GROUP BY branchcode,module,code,name,branchname";
+
     $sql .= " ORDER BY module, code, branchcode";
 
     return $dbh->selectall_arrayref($sql, { Slice => {} }, @args);