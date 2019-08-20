bywatersolutions/bywater-koha

Bug 14570: Make it possible to add multiple guarantors to a record
This patch adds the ability to set an unlimited number of guarantors
for a given patron. As before, each guarantor may be linked to another
Koha patron, and all the behavior that applies to a given guarantor
remains the same.

Test Plan:
 1) Apply this patch
 2) Run updatedatabase.pl
 3) Find some patrons with guarantors, verify the still have their guarantor
 4) Test adding and removing guarantors on a patron record, both Koha users and not
 5) Verify the "Add child" button works
 6) Verify NoIssuesChargeGuarantees still works
 7) Verify tools/cleanborrowers.pl will not delete a guarantor
 8) Verify the guarantors are displayed on moremember.pl
 9) Verify the guarantor is removed by members/update-child.pl
10) Verify the guarantor is removed by misc/cronjobs/j2a.pl
11) Verify import patrons converts guarantor_id, relationship, contactfirstname,
  and contactsurname into a guarantor
12) prove t/Patron.t
13) prove t/db_dependent/Circulation.t
14) prove t/db_dependent/Circulation/NoIssuesChargeGuarantees.t
15) prove t/db_dependent/Items.t
16) prove t/db_dependent/Koha/Patrons.t
17) prove t/db_dependent/Members.t
18) prove t/db_dependent/Patron/Relationships.t

Signed-off-by: Kim Peine <kmpeine@gmail.com>

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

Koha/Patron.pm
@@ -27,15 +27,16 @@ use Text::Unaccent qw( unac_string );
 use C4::Context;
 use C4::Log;
 use Koha::Checkouts;
+use Koha::Old::Checkouts;
 use Koha::Database;
 use Koha::DateUtils;
 use Koha::Holds;
-use Koha::Old::Checkouts;
+use Koha::Patrons;
 use Koha::Patron::Categories;
+use Koha::Patron::Relationships;
 use Koha::Patron::HouseboundProfile;
 use Koha::Patron::HouseboundRole;
 use Koha::Patron::Images;
-use Koha::Patrons;
 use Koha::Virtualshelves;
 use Koha::Club::Enrollments;
 use Koha::Account;
@@ -141,19 +142,9 @@ sub category {
     return Koha::Patron::Category->_new_from_dbic( $self->_result->categorycode );
 }
 
-=head3 guarantor
-
-Returns a Koha::Patron object for this patron's guarantor
+=head3 image
 
 =cut
-
-sub guarantor {
-    my ( $self ) = @_;
-
-    return unless $self->guarantorid();
-
-    return Koha::Patrons->find( $self->guarantorid() );
-}
 
 sub image {
     my ( $self ) = @_;
@@ -161,21 +152,49 @@ sub image {
     return scalar Koha::Patron::Images->find( $self->borrowernumber );
 }
 
+=head3 library
+
+=cut
+
 sub library {
     my ( $self ) = @_;
     return Koha::Library->_new_from_dbic($self->_result->branchcode);
 }
 
-=head3 guarantees
+=head3 guarantor_relationships
 
-Returns the guarantees (list of Koha::Patron) of this patron
+Returns Koha::Patron::Relationships object for this patron's guarantors
+
+Returns the set of relationships for the patrons that are guarantors for this patron.
+
+This is returned instead of a Koha::Patron object because the guarantor
+may not exist as a patron in Koha. If this is true, the guarantors name
+exists in the Koha::Patron::Relationship object and will have no guarantor_id.
 
 =cut
 
-sub guarantees {
-    my ( $self ) = @_;
+sub guarantor_relationships {
+    my ($self) = @_;
 
-    return Koha::Patrons->search( { guarantorid => $self->borrowernumber } );
+    return Koha::Patron::Relationships->search( { guarantee_id => $self->id } );
+}
+
+=head3 guarantee_relationships
+
+Returns Koha::Patron::Relationships object for this patron's guarantors
+
+Returns the set of relationships for the patrons that are guarantees for this patron.
+
+The method returns Koha::Patron::Relationship objects for the sake
+of consistency with the guantors method.
+A guarantee by definition must exist as a patron in Koha.
+
+=cut
+
+sub guarantee_relationships {
+    my ($self) = @_;
+
+    return Koha::Patron::Relationships->search( { guarantor_id => $self->id } );
 }
 
 =head3 housebound_profile
@@ -213,23 +232,22 @@ Returns the siblings of this patron.
 =cut
 
 sub siblings {
-    my ( $self ) = @_;
+    my ($self) = @_;
 
-    my $guarantor = $self->guarantor;
+    my @guarantors = $self->guarantor_relationships()->guarantors();
 
-    return unless $guarantor;
+    return unless @guarantors;
 
-    return Koha::Patrons->search(
-        {
-            guarantorid => {
-                '!=' => undef,
-                '=' => $guarantor->id,
-            },
-            borrowernumber => {
-                '!=' => $self->borrowernumber,
-            }
-        }
-    );
+    my @siblings =
+      map { $_->guarantee_relationships()->guarantees() } @guarantors;
+
+    return unless @siblings;
+
+    my %seen;
+    @siblings =
+      grep { !$seen{ $_->id }++ && ( $_->id != $self->id ) } @siblings;
+
+    return wantarray ? @siblings : Koha::Patrons->search( { borrowernumber => { -in => [ map { $_->id } @siblings ] } } );
 }
 
 =head3 merge_with

KohaAloha/Koha2

Signed-off-by: Agustin Moyano <agustinmoyano@theke.io>
Signed-off-by: Liz Rea <wizzyrea@gmail.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

Koha/Patron.pm
@@ -27,8 +27,10 @@ use Text::Unaccent qw( unac_string );
 
 use C4::Context;
 use C4::Log;
+use Koha::Account;
 use Koha::AuthUtils;
 use Koha::Checkouts;
+use Koha::Club::Enrollments;
 use Koha::Database;
 use Koha::DateUtils;
 use Koha::Exceptions::Password;
@@ -39,12 +41,11 @@ use Koha::Patron::Categories;
 use Koha::Patron::HouseboundProfile;
 use Koha::Patron::HouseboundRole;
 use Koha::Patron::Images;
+use Koha::Patron::Relationships;
 use Koha::Patrons;
-use Koha::Virtualshelves;
-use Koha::Club::Enrollments;
-use Koha::Account;
 use Koha::Subscription::Routinglists;
 use Koha::Token;
+use Koha::Virtualshelves;
 
 use base qw(Koha::Object);
 
@@ -372,19 +373,9 @@ sub category {
     return Koha::Patron::Category->_new_from_dbic( $self->_result->categorycode );
 }
 
-=head3 guarantor
-
-Returns a Koha::Patron object for this patron's guarantor
+=head3 image
 
 =cut
-
-sub guarantor {
-    my ( $self ) = @_;
-
-    return unless $self->guarantorid();
-
-    return Koha::Patrons->find( $self->guarantorid() );
-}
 
 sub image {
     my ( $self ) = @_;
@@ -392,21 +383,57 @@ sub image {
     return scalar Koha::Patron::Images->find( $self->borrowernumber );
 }
 
+=head3 library
+
+Returns a Koha::Library object representing the patron's home library.
+
+=cut
+
 sub library {
     my ( $self ) = @_;
     return Koha::Library->_new_from_dbic($self->_result->branchcode);
 }
 
-=head3 guarantees
+=head3 guarantor_relationships
 
-Returns the guarantees (list of Koha::Patron) of this patron
+Returns Koha::Patron::Relationships object for this patron's guarantors
+
+Returns the set of relationships for the patrons that are guarantors for this patron.
+
+This is returned instead of a Koha::Patron object because the guarantor
+may not exist as a patron in Koha. If this is true, the guarantors name
+exists in the Koha::Patron::Relationship object and will have no guarantor_id.
 
 =cut
 
-sub guarantees {
-    my ( $self ) = @_;
+sub guarantor_relationships {
+    my ($self) = @_;
 
-    return Koha::Patrons->search( { guarantorid => $self->borrowernumber }, { order_by => { -asc => ['surname','firstname'] } } );
+    return Koha::Patron::Relationships->search( { guarantee_id => $self->id } );
+}
+
+=head3 guarantee_relationships
+
+Returns Koha::Patron::Relationships object for this patron's guarantors
+
+Returns the set of relationships for the patrons that are guarantees for this patron.
+
+The method returns Koha::Patron::Relationship objects for the sake
+of consistency with the guantors method.
+A guarantee by definition must exist as a patron in Koha.
+
+=cut
+
+sub guarantee_relationships {
+    my ($self) = @_;
+
+    return Koha::Patron::Relationships->search(
+        { guarantor_id => $self->id },
+        {
+            prefetch => 'guarantee',
+            order_by => { -asc => [ 'guarantee.surname', 'guarantee.firstname' ] },
+        }
+    );
 }
 
 =head3 housebound_profile
@@ -444,23 +471,22 @@ Returns the siblings of this patron.
 =cut
 
 sub siblings {
-    my ( $self ) = @_;
+    my ($self) = @_;
 
-    my $guarantor = $self->guarantor;
+    my @guarantors = $self->guarantor_relationships()->guarantors();
 
-    return unless $guarantor;
+    return unless @guarantors;
 
-    return Koha::Patrons->search(
-        {
-            guarantorid => {
-                '!=' => undef,
-                '=' => $guarantor->id,
-            },
-            borrowernumber => {
-                '!=' => $self->borrowernumber,
-            }
-        }
-    );
+    my @siblings =
+      map { $_->guarantee_relationships()->guarantees() } @guarantors;
+
+    return unless @siblings;
+
+    my %seen;
+    @siblings =
+      grep { !$seen{ $_->id }++ && ( $_->id != $self->id ) } @siblings;
+
+    return wantarray ? @siblings : Koha::Patrons->search( { borrowernumber => { -in => [ map { $_->id } @siblings ] } } );
 }
 
 =head3 merge_with
@@ -1417,6 +1443,34 @@ sub _anonymize_column {
         $val = $nullable ? undef : dt_from_string;
     }
     $self->$col($val);
+}
+
+=head3 add_guarantor
+
+    my @relationships = $patron->add_guarantor(
+        {
+            borrowernumber => $borrowernumber,
+            relationships  => $relationship,
+        }
+    );
+
+    Adds a new guarantor to a patron.
+
+=cut
+
+sub add_guarantor {
+    my ( $self, $params ) = @_;
+
+    my $guarantor_id = $params->{guarantor_id};
+    my $relationship = $params->{relationship};
+
+    return Koha::Patron::Relationship->new(
+        {
+            guarantee_id => $self->id,
+            guarantor_id => $guarantor_id,
+            relationship => $relationship
+        }
+    )->store();
 }
 
 =head2 Internal methods