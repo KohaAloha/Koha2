Koha-Community/Koha

commit bca4453c50d8b5b1d3c7029ded2b522a9a2bf868

Bug 23624: (QA follow-up) Optimize even more
This patch makes counting the results have no memory footprint by
leveraging on the DB to count the rows.

To test:
- Without this path, run:
  $ kshell
 k$ prove t/db_dependent/Reports/Guided.t
=> SUCCESS: Tests pass
- Apply this patch
- Run:
 k$ prove t/db_dependent/Reports/Guided.t
=> SUCCESS: Tests still pass!

Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
Signed-off-by: Liz Rea <wizzyrea@gmail.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

C4/Reports/Guided.pm
@@ -418,14 +418,19 @@ sub get_criteria {
 
 sub nb_rows {
     my $sql = shift or return;
-    my $sth = C4::Context->dbh->prepare($sql);
-    $sth->execute();
-    my $n = 0;
-    # Loop through the complete results, fetching 1,000 rows at a time.  This
-    # lowers memory requirements but increases execution time.
-    while (my $rows = $sth->fetchall_arrayref(undef, 1000)) {
-        $n += @$rows;
+
+    my $derived_name = 'xxx';
+    # make sure the derived table name is not already used
+    while ( $sql =~ m/$derived_name/ ) {
+        $derived_name .= 'x';
     }
+    my $sth = C4::Context->dbh->prepare(qq{
+        SELECT COUNT(*) FROM
+        ( $sql ) $derived_name
+    });
+    $sth->execute();
+    my $n = $sth->fetch->[0];
+
     return $n;
 }
 

KohaAloha/Koha2

commit 19682607eaa69e0b121f954b87a3ec3629bbd4da

Bug 23982: (bug 23624 follow-up) Handle SQL error caused by derived table
Caused by
  commit bca4453c50d8b5b1d3c7029ded2b522a9a2bf868
  Bug 23624: (QA follow-up) Optimize even more

A report like:
SELECT * FROM issues JOIN borrowers USING (borrowernumber)

will have two borrowernumber columns - SQL will give us there rsults,
but if we try to wrap them in a SELECT COUNT(*) FROM (report) it throws
a duplicated column error.

This patch suggests to execute the query the old way if the derived
table optimization failed.

Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

C4/Reports/Guided.pm
@@ -425,19 +425,37 @@ sub nb_rows {
         $derived_name .= 'x';
     }
 
-    my $sth = C4::Context->dbh->prepare(qq{
-        SELECT COUNT(*) FROM
-        ( $sql ) $derived_name
-    });
 
-    $sth->execute();
+    my $dbh = C4::Context->dbh;
+    my ( $sth, $n );
 
-    if ( $sth->errstr ) {
-        return 0;
+    my $RaiseError = $dbh->{RaiseError};
+    my $PrintError = $dbh->{PrintError};
+    $dbh->{RaiseError} = 1;
+    $dbh->{PrintError} = 0;
+    eval {
+        $sth = $dbh->prepare(qq{
+            SELECT COUNT(*) FROM
+            ( $sql ) $derived_name
+        });
+
+        $sth->execute();
+    };
+    $dbh->{RaiseError} = $RaiseError;
+    $dbh->{PrintError} = $PrintError;
+    if ($@) { # To catch "Duplicate column name" caused by the derived table, or any other syntax error
+        $sth = $dbh->prepare($sql);
+        $sth->execute;
+        # Loop through the complete results, fetching 1,000 rows at a time.  This
+        # lowers memory requirements but increases execution time.
+        while (my $rows = $sth->fetchall_arrayref(undef, 1000)) {
+            $n += @$rows;
+        }
+        return $n;
     }
-    else {
-       return $sth->fetch->[0];
-    }
+
+    my $results = $sth->fetch;
+    return $results ? $results->[0] : 0;
 }
 
 =head2 execute_query