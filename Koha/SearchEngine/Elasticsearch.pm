79bef054502b6b57459b88554aba9b622405c0aa
Mason James <mtj@kohaaloha.com>
2020-02-14 22:24:40 +1300

    modified:   Koha/SearchEngine/Elasticsearch.pm

--- Koha/SearchEngine/Elasticsearch.pm
+++ Koha/SearchEngine/Elasticsearch.pm
@@ -144,7 +144,7 @@ sub get_elasticsearch_params {
     $es->{index_name} .= '_' . $self->index;
 
     $es->{key_prefix} = 'es_';
-    $es->{client} //= '6_0::Direct';
+#    $es->{client} //= '6_0::Direct';
     $es->{cxn_pool} //= 'Static';
     $es->{request_timeout} //= 60;
 
